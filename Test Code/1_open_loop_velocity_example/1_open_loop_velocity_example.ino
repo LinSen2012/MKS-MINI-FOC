bcfc287fd9589176da2d2a8bda16fb3d9945b6ed
linsen2008 <linsen2008@163.com>
2026-04-18 16:01:14 +0800

    *)In Mega2560 ,add macro #define SIMPLEFOC_DISABLE_ATMEGA2560_MCU_WARNING to eliminate warnning r)linsen

--- Test Code/1_open_loop_velocity_example/1_open_loop_velocity_example.ino
+++ Test Code/1_open_loop_velocity_example/1_open_loop_velocity_example.ino
@@ -5,6 +5,13 @@
 /// The default power supply voltage of the program is 12V.
 /// Please remember to modify the voltage_power_supply , voltage_limit variable values when using other voltages for power supply
 
+#define SIMPLEFOC_DISABLE_ATMEGA2560_MCU_WARNING 
+/* 
+eliminate warnning of below :
+ "note: #pragma message: SimpleFOC: compiling for Arduino/ATmega2560 or Arduino/ATmega1280
+ #pragma message("SimpleFOC: compiling for Arduino/ATmega2560 or Arduino/ATmega1280")"
+ */
+ 
 #include <SimpleFOC.h>
 
 BLDCMotor motor = BLDCMotor(7);                               //According to the selected motor, modify the number of pole pairs here, the value in BLDCMotor()
